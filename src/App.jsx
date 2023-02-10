2fd98313889c2fc223f054c883b50df2b5b8edbe
RiP3rQ <rafalpompa2000@gmail.com>
2023-02-10 21:41:37 +0100

    Themes light/dark mode setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import "./App.css";
+
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+import TopBar from "./scenes/global/TopBar";
 
 function App() {
+  const [theme, colorMode] = useMode();
   const [count, setCount] = useState(0);
 
-  return <div className="App"></div>;
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline>
+          <div className="app">
+            <main className="content">
+              <TopBar />
+            </main>
+          </div>
+        </CssBaseline>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
 
 export default App;
